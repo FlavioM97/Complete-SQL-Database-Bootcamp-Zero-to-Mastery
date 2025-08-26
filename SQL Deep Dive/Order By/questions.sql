0180d60180b282b32068962e7fc7f652918d1de1
FlavioM97 <flavioalexandre97@hotmail.com>
2025-08-26 10:28:27 +0100

    Update questions.sql

--- SQL Deep Dive/Order By/questions.sql
+++ SQL Deep Dive/Order By/questions.sql
@@ -4,6 +4,8 @@
 * Question: Sort employees by first name ascending and last name descending
 */
 
+SELECT * FROM employees
+ORDER BY first_name, last_name DESC;
 
 /*
 * DB: Employees
@@ -11,9 +13,13 @@
 * Question: Sort employees by age
 */
 
+SELECT * FROM employees
+ORDER BY hire_date;
 
 /*
 * DB: Employees
 * Table: employees
 * Question: Sort employees who's name starts with a "k" by hire_date
 */
+SELECT first_name = 'k%' FROM employees
+ORDER BY hire_date;
