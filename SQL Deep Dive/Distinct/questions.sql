dc19d9c58587dc9dd4b9988bfa7a7dbebb65303b
FlavioM97 <flavioalexandre97@hotmail.com>
2025-08-26 10:12:12 +0100

    Update questions.sql

--- SQL Deep Dive/Distinct/questions.sql
+++ SQL Deep Dive/Distinct/questions.sql
@@ -4,7 +4,7 @@
 * Question: What unique titles do we have?
 */
 
-SELECT * FROM titles;
+SELECT DISTINCT titles.title FROM titles
 
 
 /*
@@ -13,7 +13,7 @@ SELECT * FROM titles;
 * Question: How many unique birth dates are there?
 */
 
-SELECT * FROM employees;
+SELECT DISTINCT birth_date FROM employees
 
 /*
 * DB: World
@@ -22,5 +22,5 @@ SELECT * FROM employees;
 * Make sure there are no nulls
 */
 
-SELECT * FROM country;
+SELECT DISTINCT lifeexpectancy FROM country
 
